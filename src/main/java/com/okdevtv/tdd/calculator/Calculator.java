1dc01b5cd682101fa5e7ce15106304f9ab47fd26
CSH <ssunghwan1@gmail.com>
2022-03-15 15:18:11 +0900

    ✨ add #1

---
+++ src/main/java/com/okdevtv/tdd/calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.okdevtv.tdd.calculator;
+
+public class Calculator {
+
+	public static int add(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/okdevtv/tdd/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.okdevtv.tdd.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		assertEquals(3, Calculator.add(1,2));
+	}
+	// testSubtract
+	// testMultiply
+	// testDivide
+
+}
